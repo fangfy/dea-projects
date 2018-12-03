0c8c975280d46045c50e8fbc9b4f798e8de2010f
Fang Yuan <fxy120@vdi-n13.novalocal>
2018-12-03 14:54:32 +1100

    Updated

--- cloud_and_shadow/mosaic.py
+++ cloud_and_shadow/mosaic.py
@@ -98,3 +98,48 @@ for idx, band in enumerate(bands):
     cmd= 'gdaladdo -r NEAREST -ro --config COMPRESS_OVERVIEW DEFLATE --config PREDICTOR_OVERVIEW 2 --config ZLEVEL 9 %s 4 16 64'%tifname
     subprocess.call(cmd, shell=True)
 
+
+#make a smoothed cloud mask to identify problem areas
+import numpy as np
+from scipy.ndimage import median_filter
+from functools import partial
+from dask.array import coarsen
+from dask import delayed
+
+# 25 m pixel
+# downsample to 5 km
+# median over 100 km
+
+pixel =25
+sampling = 5000
+filtersize = 100000
+# for downsample
+
+block_size = int(sampling/pixel)
+
+if not os.path.exists('cloud_fmask_percent_downsample.nc'):
+    orig_data_array = xr.open_rasterio('cloud_fmask_percent.tif', chunks={'x':4000, 'y':4000})
+    data = orig_data_array.chunk().data.squeeze().astype(np.float32)
+    data[data==255]=np.nan
+    coords = {'band': orig_data_array.coords['band'].values, 
+              'y': (orig_data_array.y[::block_size].values+orig_data_array.y[block_size-1::block_size].values)/2,
+              'x': (orig_data_array.x[::block_size].values+orig_data_array.x[block_size-1::block_size].values)/2}
+    new_data = coarsen(np.median, data, {0: block_size, 1:block_size})
+    new_array = xr.DataArray(new_data[np.newaxis,:,:], dims=('band','y','x'), coords=coords).to_dataset(name='cloud_percent')
+    result = new_array.to_netcdf('cloud_fmask_percent_downsample.nc',compute=False)
+    result.compute()
+
+if not os.path.exists('cloud_fmask_percent_smooth.nc'):
+    # for median filter
+    rad = int(filtersize/2/sampling)
+    y,x = np.ogrid[-1*rad: rad+1, -1*rad: rad+1]
+    footprint = x**2.+y**2. <= rad**2.
+    smooth = partial(median_filter, footprint=footprint)
+    
+    orig_data_array = xr.open_rasterio('cloud_fmask_percent_downsample.nc', chunks={'x':4000, 'y':4000})
+    data = orig_data_array.chunk().data.squeeze().astype(np.float32)
+    data[data==255]=np.nan
+    new_data = data.map_overlap(smooth, depth=rad)
+    new_array = xr.DataArray(new_data[np.newaxis,:,:], orig_data_array.coords).to_dataset(name='cloud_percent')
+    result= new_array.to_netcdf('cloud_fmask_percent_smooth.nc', compute=False)
+    result.compute()
